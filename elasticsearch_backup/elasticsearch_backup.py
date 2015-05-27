82428053e7de2a255f8ffca489a6cbbd576b1b83
Vedarth Kulkarni <vedarthk@vedarthz.in>
2015-05-27 21:08:23 +0530

    use polling to get status of the snapshot process

--- elasticsearch_backup/elasticsearch_backup.py
+++ elasticsearch_backup/elasticsearch_backup.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import json
+import time
 import requests
 import datetime
 
@@ -30,25 +31,69 @@ def create_s3_repo(node, repo, bucket):
     _sys_print(' [OK]\n')
 
 
+def poll_snapshot_status(snapshot_uri):
+    status_uri = "{snapshot_uri}/_status".format(snapshot_uri=snapshot_uri)
+    failure_count = 0
+    sleep_time = 1
+    print "Files:"
+    counter = 0
+    spinner = ['|', '/', '-', '\\']
+    while True:
+        r = requests.get(status_uri)
+
+        if r.status_code == 200:
+            snapshot_status = r.json()
+            status = snapshot_status['snapshots'][0]['state']
+            if status == 'STARTED':
+                stats = snapshot_status['snapshots'][0]['stats']
+                _sys_print(
+                    '\r{}/{} '.format(
+                        stats['processed_files'], stats['number_of_files']))
+            elif status == 'SUCCESS':
+                return print_success_message(snapshot_status)
+            else:
+                raise Exception('Bad snapshot status: {}'.format(r.json()))
+        else:
+            failure_count += 1
+            sleep_time = sleep_time * 2
+
+        if failure_count > 10:
+            raise Exception(
+                'Bad status code {} {}'.format(r.status_code, r.content))
+        counter += 1
+        _sys_print(spinner[counter % len(spinner)])
+        time.sleep(sleep_time)
+
+
 def create_snapshot(node, repo):
     date_stamp = datetime.datetime.now().date().strftime('%Y%m%d')
     _sys_print('Creating snapshot for {} ...'.format(date_stamp))
 
-    snapshot_uri = '{repo}/snapshot_{date}?wait_for_completion=true'.format(
+    snapshot_uri = '{repo}/snapshot_{date}'.format(
         repo=REPO_URI.format(es_node=node, repo=repo), date=date_stamp)
 
     r = requests.put(snapshot_uri)
 
     result = r.json()
     assert r.status_code == 200, result
-    if result['snapshot']['state'] != 'SUCCESS':
+
+    if not result['accepted']:
         raise Exception('Snapshot Failed\nReason: {}'.format(result))
 
     _sys_print(' [OK]\n')
+
+    poll_snapshot_status(snapshot_uri)
+
+
+def print_success_message(snapshot_status):
+    stats = snapshot_status['snapshots'][0]['stats']
+    shard_stats = snapshot_status['snapshots'][0]['shards_stats']
+    print '\n'
     print '-'*80
-    print 'Stats: Shards - {}/{}; Duration - {}s'.format(
-        result['snapshot']['shards']['successful'], result['snapshot']['shards']['total'],
-        result['snapshot']['duration_in_millis']/100)
+    print 'Stats: Files: {}/{}; Shards - {}/{}; Duration - {}s'.format(
+        stats['processed_files'], stats['number_of_files'],
+        shard_stats['done'], shard_stats['total'],
+        stats['time_in_millis']/100)
     print '-'*80
 
 
